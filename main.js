b70d9bed786850dd6031e258a00632065de55f66
Federico Godoy <federicoegodoy@gmail.com>
2024-06-03 21:36:49 -0300

    Javascript - Ejercicio 7

---
+++ main.js
@@ -0,0 +1,190 @@
+import './style.css'
+
+
+/* 1. Declaración y Asignación: Declara una variable llamada nombre y asígnale tu nombre como valor.
+ */
+
+/*  const nombre = 'Federico' 
+console.log(nombre) */
+
+
+
+
+
+/*  2. Tipos de Datos: Crea variables para almacenar un número entero (edad), un número decimal (precio), una cadena de texto (ciudad) y un valor booleano (esEstudiante). */
+
+
+/*const edad = 28
+const precio = 14.50
+const ciudad = 'Buenos Aires'
+const esEstudiante = true
+
+console.log(edad)
+console.log(precio)
+console.log(ciudad)
+console.log(esEstudiante) */
+
+
+
+
+
+
+
+/* 3. Concatenación: Declara dos variables (nombre, apellido) y muestra un mensaje que diga "Hola, [nombre] [apellido]". */
+
+/* const nombre = 'Federico'
+const apellido = 'Godoy'
+console.log('Hola, ' + nombre + apellido) */
+
+
+
+
+
+/* 4. Operaciones Matemáticas: Declara dos variables numéricas (num1, num2) y realiza las operaciones básicas (suma, resta, multiplicación, división) con ellas. */
+
+
+/* const num1 = 20
+const num2 = 5
+const resultadoSuma = num1 + num2
+
+  console.log(resultadoSuma)
+
+
+const resultadoResta = num1 - num2
+
+  console.log(resultadoResta)
+
+
+const resultadoMultiplicacion= num1 * num2
+
+  console.log(resultadoMultiplicacion) 
+
+
+const resultadoDivision = num1 / num2
+
+  console.log(resultadoDivision) */
+
+
+
+
+/* 5. Incremento y Decremento: Declara una variable (contador) y utiliza los operadores de incremento (++) y decremento (--) para modificar su valor. */
+
+/* let contadorIncremento = 20
+contadorIncremento++
+console.log(contadorIncremento)
+
+let contadorDecremento =  5
+contadorDecremento--
+console.log(contadorDecremento) */
+
+
+
+/* 6. Prompt y Alert: Pide al usuario que ingrese su nombre mediante prompt() y luego muestra un mensaje de bienvenida con alert(). */
+
+/* const nombreDeUsueario = prompt('Ingrese su nombre')
+const saludo = alert('Bienvenido ' + nombreDeUsueario) */
+
+
+
+
+/* 7. Cálculo de Área: Pide al usuario el radio de un círculo y calcula su área (π * radio^2). */
+
+
+/* const pi = 3.14
+
+
+const radioDeCirculo = prompt('Ingrese el radio del circulo')
+const respuesta = alert( 'El radio del circulo es ' + pi * radioDeCirculo * radioDeCirculo) */
+
+/* 
+
+
+
+
+8. Conversión de Unidades: Pide al usuario una temperatura en grados Celsius y conviértela a Fahrenheit. */
+
+
+/* const temperaturaCelsius = prompt('Ingrese la temperatura en grados Celsius')
+const respuesta= alert('La temperatura Fahrenheit es de ' + (temperaturaCelsius * 1.8 + 32))  */
+
+
+
+
+
+
+/* 9. Cálculo de Promedio: Pide al usuario tres números y calcula su promedio. */
+
+/* let valor1String = prompt("Ingrese el primer valor")
+let valor1 = parseInt(valor1String)
+console.log(valor1)
+let valor2String = prompt("Ingrese el segundo valor")
+let valor2 = parseInt(valor2String)
+console.log(valor2)
+let valor3String = prompt("Ingrese el tercer valor")
+let valor3 = parseInt(valor3String)
+console.log(valor3)
+
+
+let promedio = (valor1 + valor2 + valor3)
+console.log(promedio) */
+
+
+/* const promedio = alert('El promedio es de ' + valor1 + valor2 + valor3 ) */
+
+
+
+/* 10. Operador Ternario: Utiliza el operador ternario para determinar si un número es positivo o negativo. */
+
+/* let numero = prompt('Ingrese un número')
+
+let resultado = (numero > 0) ? 'Positivo' : 'Negativo'
+console.log(resultado) */
+
+
+
+
+/* 11. Condicional if-else: Pide al usuario su edad y determina si es mayor o menor de edad. */
+
+
+/* let edad = prompt('Ingrese su edad')
+
+if (edad >= 18) {
+  console.log('Usted es mayor de edad')
+} else {21
+  console.log('Usted no es mayor de edad')
+} */
+
+
+
+
+
+/* 12. Condicional switch: Pide al usuario un número del 1 al 7 y muestra el día de la semana correspondiente. */
+
+
+/* let numero =prompt('Ingrese un número que corresponderá a un día de la semana')
+
+switch (numero) {
+  case '1':
+    console.log('Día LUNES')
+    break;
+    case '2':
+    console.log('Día MARTES')
+    break;
+  case '3':
+    console.log('Día MIERCOLES')
+    break;
+  case '4':
+    console.log('Día JUEVES')
+    break;
+  case '5':
+    console.log('Día VIERNES')
+    break;
+  case '6':
+    console.log('Día SÁBADO')
+    break;
+  case '7':
+    console.log('Día DOMINGO')
+    break;
+  default:
+    break;
+} */
